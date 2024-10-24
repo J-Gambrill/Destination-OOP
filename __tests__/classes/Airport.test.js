77bf78461ab4d7e796562d5e7993d09aaf98f8fe
James Gambrill <jameswgambrill@icloud.com>
2024-10-24 17:29:05 +0100

    Airport complete

--- __tests__/classes/Airport.test.js
+++ __tests__/classes/Airport.test.js
@@ -2,12 +2,23 @@ const Airport = require('./classes/Airport.js')
 const { describe, it, expect } = require('@jest/globals')
 
 
-describe('Person', () => {
+describe('Airport', () => {
     
-    const p1 = new Person(100, 1234, 'James')
-    
+    const a1 = new Airport('Ibiza airport', 'IBZ')
+    const plane1 = { company: 'BA', destination: 'UK' }; // Mock plane object
+
     test('Is an instance created?', () => {
-        expect(b1).toBeInstanceOf(Bag)
+        expect(a1).toBeInstanceOf(Airport)
+    })
+
+    test('Is a name + planes created?', () => {
+        expect(a1.name).toBe('Ibiza airport')
+        expect(a1.planes).toEqual([])
+    })
+
+    test('Is a plane created?', () => {
+        a1.addPlane(plane1)
+        expect(a1.getPlanes()).toEqual([plane1])
     })
 
 
--- classes/Airport.js
+++ classes/Airport.js
@@ -1,5 +1,20 @@
 class Airport {
 
+        #airportCode
+    constructor(name, airportCode, planes = []){
+        this.name = name
+        this.planes = planes
+        this.#airportCode = airportCode
+    }
+
+    getPlanes(){
+        return this.planes
+    }
+
+    addPlane(plane){
+        this.planes.push(plane)
+    }
+    
 
 }
 
